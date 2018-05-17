2e4d1bdcf78d04d8682777fa9f750f36d16fe4a1
jeaye <contact@jeaye.com>
2018-05-16 22:45:13 -0700

    Add the most basic packing code

--- src/com/jeaye/easy_pack.clj
+++ src/com/jeaye/easy_pack.clj
@@ -2,5 +2,21 @@
   (:gen-class)
   (:require [mikera.image.core :as img]))
 
+(defn copy-pixels! [output-image images]
+  (loop [imgs images
+         x 0]
+    (when-let [img (first imgs)]
+      (let [img-width (img/width img)
+            img-height (img/height img)
+            sub-output (img/sub-image output-image x 0 img-width img-height)]
+        (img/set-pixels sub-output (img/get-pixels img))
+        (recur (rest imgs) (+ x img-width))))))
+
 (defn -main [& args]
-  (println "Hello, World!"))
+  (let [images (mapv img/load-image args)
+        full-width (reduce #(+ %1 (img/width %2)) 0 images)
+        full-height (apply max (map img/height images))
+        output-image (img/new-image full-width full-height)]
+    (println full-width "x" full-height)
+    (copy-pixels! output-image images)
+    (img/show output-image)))

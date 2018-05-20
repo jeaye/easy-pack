3884a1d620aae9577f444489959a6779c23ab473
jeaye <contact@jeaye.com>
2018-05-19 22:45:13 -0700

    Rewrite layout building

--- src/com/jeaye/easy_pack.clj
+++ src/com/jeaye/easy_pack.clj
@@ -12,30 +12,26 @@
 (defn build-layout [image-infos]
   (loop [infos image-infos
          x 0
-         acc {}]
+         acc {:layout {}}]
     (if-let [info (first infos)]
       (let [{:keys [image path]} info
             img-width (img/width image)
             img-height (img/height image)]
         (recur (rest infos)
                (+ x img-width)
-               (assoc acc path {:image image
-                                :width img-width
-                                :height img-height
-                                :x x
-                                :y 0})))
+               (-> acc
+                   (update :width + img-width)
+                   (update :height max img-height)
+                   (assoc-in [:layout path] {:image image
+                                             :width img-width
+                                             :height img-height
+                                             :x x
+                                             :y 0}))))
       acc)))
-
-(defn combine! [output-fns image-infos]
-  (let [full-width (reduce #(+ %1 (-> %2 :image img/width)) 0 image-infos)
-        full-height (apply max 0 (map #(-> % :image img/height) image-infos))]
-    {:width full-width
-     :height full-height
-     :layout (build-layout image-infos)}))
 
 ; --outputs png,css,json,edn
 (defn -main [& args]
   (let [image-infos (mapv load-image! args)
         output-fns [image/output css/output]
-        output (combine! output-fns image-infos)]
+        layout (build-layout image-infos)]
     (img/save (:image output) "output.png" :quality 1.0 :progressive nil)))

a2a77ddde996d3fb83f1f5f2099dc7c6408e31f3
jeaye <contact@jeaye.com>
2018-05-22 23:23:23 -0700

    Add tools.cli dep

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "jank license"
             :url "https://upload.jeaye.com/jank-license"}
   :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/tools.cli "0.3.7"]
                  [me.raynes/fs "1.4.6"]
                  [net.mikera/imagez "0.12.0"]]
   :main ^:skip-aot com.jeaye.easy-pack

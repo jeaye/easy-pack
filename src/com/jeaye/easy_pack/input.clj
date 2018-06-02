1553425b9c04c6fe5fd03723142d6e1eddee5c6d
jeaye <contact@jeaye.com>
2018-06-01 18:51:14 -0700

    Spec out input ns

--- src/com/jeaye/easy_pack/input.clj
+++ src/com/jeaye/easy_pack/input.clj
@@ -1,6 +1,13 @@
 (ns com.jeaye.easy-pack.input
-  (:require [mikera.image.core :as img]))
+  (:require [clojure.spec.alpha :as s]
+            [mikera.image.core :as img]
+            [orchestra.core :refer [defn-spec]]
+            [com.jeaye.easy-pack.util :as util]))
 
-(defn load-image! [path]
+(s/def ::image #(instance? java.awt.image.BufferedImage %))
+(s/def ::loaded-image (s/keys :req-un [::image ::util/path]))
+
+(defn-spec load-image! ::loaded-image
+  [path ::util/path]
   {:image (img/load-image path)
    :path path})

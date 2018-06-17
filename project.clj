5b855139b847fe2328480b1ec13caa29e71efec5
jeaye <contact@jeaye.com>
2018-06-17 12:35:52 -0700

    Add graal project config

--- project.clj
+++ project.clj
@@ -8,9 +8,19 @@
                  [orchestra "2017.11.12-1"]
                  [medley "1.0.0"]
                  [me.raynes/fs "1.4.6"]
-                 [net.mikera/imagez "0.12.0"]]
+                 [imagez "1.6.1" :exclusions [;net.mikera/mathz
+                                              ;net.mikera/randomz
+                                              ;net.mikera/mikera-gui
+                                              ;org.imgscalr/imgscalr-lib
+                                              ]]]
+  :plugins [[io.taylorwood/lein-native-image "0.2.0"]]
   :main ^:skip-aot com.jeaye.easy-pack
   :target-path "target/%s"
+  :native-image {:name "easy-pack"
+                 :opts ["--verbose"
+                        "--no-server"
+                        "-H:+ReportUnsupportedElementsAtRuntime"]
+                 :graal-bin "/usr/lib/jvm/java-8-graal/bin"}
   :profiles {:dev {:source-paths ["src/" "test/"]
                    :plugins [[com.jakemccrary/lein-test-refresh "0.22.0"]
                              [lein-cloverage "1.0.10"]]
